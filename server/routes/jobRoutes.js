3c81f61a68e7b59bf258c59b11328f2db60f220b
Aytac <aytacibrahimova2612@gmail.com>
2024-02-10 23:55:17 +0400

    first commit

--- server/routes/jobRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getJobs, createJob, updateJob, deleteJob } = require("../controllers/jobController");
-const router = express.Router();
-
-router.route("/").get(getJobs).post(createJob)
-router.route("/:id").put(updateJob).delete(deleteJob)
-
-module.exports = router
